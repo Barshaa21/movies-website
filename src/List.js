095be0a74f0761264f6df025ac3d3ed82b0b8085
Barshaa21 <117267581+Barshaa21@users.noreply.github.com>
2023-02-04 10:54:15 +0545

    Update List.js

--- src/List.js
+++ src/List.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-export default function List({ title, year, type, image }) {
+export default function List({ title, year, type, image}) {
     return (
-        <div >
-            <h1>{title}</h1>
-            {/* <image src={image}/> */}
+        <div className='list'>
+            <h1>{title.slice(0,18)}</h1>
             <div>
                 <img
                     src={image !== "N/A" ? image : "https://via.placeholder.com/400"}
@@ -13,7 +12,6 @@ export default function List({ title, year, type, image }) {
             </div>
             <p>{year}</p>
             <p>{type}</p>
-            {/* <p>{Year}</p> */}
         </div>
     )
 }
