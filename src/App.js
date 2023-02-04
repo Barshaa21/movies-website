9cb3937ce8b9d9662e22e798df80d1ad31ba3ba9
Barshaa21 <117267581+Barshaa21@users.noreply.github.com>
2023-02-04 10:53:14 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header';
@@ -10,19 +9,13 @@ function App() {
   const [movies, setMovies] = useState([]);
 
   const handleInputText = (e) => {
-    setSearch(e.target.value); //a and e in handle input can be different same value will be assigned in argument
-    console.log(e)//value stored within taget.value can check in inspect
-  }
+    setSearch(e.target.value); }
 
   useEffect(()=>{
-    searchMovies('test')
-  },[])//array dependencies to reserach everytime
+    searchMovies('avengers')
+  },[])
 
   const searchMovies = async (searchbytitle) => {
-    // alert(searchbytitle)
-    // const response = await fetch(`${API_URL}&s=${searchbytitle}`)
-    // const data = await response.json();
-    // console.log(data.Search);
     const a=await Opt(searchbytitle);
     setMovies(a);
   }
@@ -33,7 +26,6 @@ function App() {
 
   return (
     <div className="app" style={{ color: 'white' }}>
-      {/* <p>this is react</p> */}
       <Header />
       <div className='search'>
         <input placeholder='Search here' value={search} onChange={(a) => handleInputText(a)} />
@@ -42,21 +34,12 @@ function App() {
       {movies && movies.length > 0 ? (
         <div className='container'>
           {movies.map((movie) => (
-            // <List title={movie.title} image={movie.image} rating={movie.rating} type={movie.type} year={movie.year}/>
             <List title={movie.Title} type={movie.Type} year={movie.Year} image={movie.Poster}/>
           ))}
         </div>) :
         (<div className='container'>
           <h1>No movies found</h1>
         </div>)}
-
-
-
-      {/* // <div className='container'>
-      //   {movies.map((movie) => { */}
-      {/* //     <List title={movie.title} image={movie.image} rating={movie.rating} />
-      //   })}
-      </div> */}
 
     </div>
   );
